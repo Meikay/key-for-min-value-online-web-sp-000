2af9440f0c8c512c26a2bd2701dcaf161ddd8e9e
Meisuleen Kased <meisuleen@gmail.com>
2018-11-17 19:07:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -18,23 +18,7 @@ def key_for_min_value(name_hash)
   end
   return def_key
 end
+end
 
 
 
-
-
-
-
-
-# def key_for_min_value(name_hash)
-#   if name_hash.length == 0
-#     nil
-#   name_hash.collect do |min_val, other_val|
-#     if min_val < other_val
-#     min_val = key_min
-#   elsif key_min == min_val
-#     return key_min
-#   end
-#     end
-#   end
-# end
