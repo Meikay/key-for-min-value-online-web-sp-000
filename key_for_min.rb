92817847cdff65d39bb93b01ce755a89f6a5f153
Meisuleen Kased <meisuleen@gmail.com>
2018-11-17 19:27:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,9 +11,11 @@ def key_for_min_value(name_hash)
   def_key = 0
   #iterate over new array
   name.each do |curr_val|
-    if curr_val < def_val
-      low_val = curr_val[1]
-      low_key = curr_val[0]
+    if low_val == nil || low_val > curr_val
+      low_val = curr_val
+    # if curr_val < def_val
+    #   low_val = curr_val[1]
+    #   low_key = curr_val[0]
     end
   end
   return def_key
